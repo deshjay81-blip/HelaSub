383fbfe30ce24a20bf0c5f899f2200db04f0f30b
deshjay81-blip <deshjay81@gmail.com>
2026-05-20 09:12:51 +0530

    Create MainActivity.java

---
+++ app/src/main/java/com/helasub/app/MainActivity.java
@@ -0,0 +1,48 @@
+package com.helasub.app;
+
+import android.os.Bundle;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebResourceResponse;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import androidx.appcompat.app.AppCompatActivity;
+import java.io.ByteArrayInputStream;
+import java.util.Arrays;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView webView;
+    // Block කරන්න ඕනේ ප්‍රධාන Ad Domains ලැයිස්තුව
+    private final List<String> adDomains = Arrays.asList(
+        "doubleclick.net", "adservice.google.com", "googlesyndication.com",
+        "popads.net", "propellerads.com", "adsterra.com", "infolinks.com"
+    );
+
+    @Override
+    protected void Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        webView = new WebView(this);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setDomStorageEnabled(true);
+
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public WebResourceResponse shouldInterceptRequest(WebView view, WebResourceRequest request) {
+                String url = request.getUrl().toString();
+                for (String domain : adDomains) {
+                    if (url.contains(domain)) {
+                        // Ad එකක් අහුවුනොත් හිස් response එකක් යවා block කරයි
+                        return new WebResourceResponse("text/plain", "UTF-8", new ByteArrayInputStream("".getBytes()));
+                    }
+                }
+                return super.shouldInterceptRequest(view, request);
+            }
+        });
+
+        // ඔයාගේ වෙබ් අඩවියේ ලිපිනය
+        webView.loadUrl("https://helasub.com");
+        setContentView(webView);
+    }
+}
